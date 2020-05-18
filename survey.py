a65ae6081be9e83f79453521879c38f23dff9c75
Kyllian <kyllian.hamadou@epitech.eu>
2020-05-18 11:38:27 +0200

    Structure of the code

---
+++ survey.py
@@ -0,0 +1,14 @@
+# Python - KYLLIAN HAMADOU - D19124158
+
+class SurveyResponse:
+	def __init__(self):
+		pass
+
+
+class Survey:
+	def __init__(self):
+		pass
+
+class Controller:
+	def __init__(self):
+		pass
